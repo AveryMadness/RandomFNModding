192ea7d9722b6269a869dd33530bdf21c02f711c
AveryMadness <averyfd@shaw.ca>
2022-04-19 14:39:01 -0600

    Create FortniteGame.h

---
+++ FortniteGame.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTags.h"
+#include "GameplayTagContainer.h"
+#include "FortniteGame_Structs.h"
+#include "Engine/CurveTable.h"
+#include "Engine/Texture2D.h"
+#include "Engine/StaticMesh.h"
+#include "Math/TransformNonVectorized.h"
+#include "UObject/Class.h"
+#include "UObject/ScriptDelegates.h"
+#include "FortniteGame.generated.h"
